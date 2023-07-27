d91bcb04b98c3ee9d77d3d987b0fe75ac9208201
pinky-pig <2545589993@qq.com>
2023-07-27 16:12:27 +0800

    fix: header styles

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -3,11 +3,14 @@
     id="nav-bg"
     class="z-2 h-80px h-fit w-full flex flex-row items-center justify-between px-3.5vw py-16px text-white"
   >
-    <img
-      class="h-40px w-40px"
-      src="/favicon.svg"
-      alt=""
-    >
+    <div class="flex flex-row items-center justify-center gap-20px">
+      <img
+        class="h-40px w-40px"
+        src="/favicon.svg"
+        alt=""
+      >
+      <span class="cursor-default text-[24px] font-bold tracking-[2px] text-[#a4b2c1]">Superellipse SVG</span>
+    </div>
 
     <div class="grid h-40px w-40px place-items-center">
       <a href="https://github.com/pinky-pig/superellipse-svg" target="_blank">
