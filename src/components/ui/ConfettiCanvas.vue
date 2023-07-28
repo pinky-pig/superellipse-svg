3297f18206ce2296240f824b096be6b6abb1e732
pinky-pig <2545589993@qq.com>
2023-07-28 14:58:51 +0800

    fix: confettiCanvas z-index upup

--- src/components/ui/ConfettiCanvas.vue
+++ src/components/ui/ConfettiCanvas.vue
@@ -1,6 +1,6 @@
 <template>
   <canvas
     id="confetti"
-    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; pointer-events: none;"
   />
 </template>
