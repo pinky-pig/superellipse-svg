cb9595c65f8f1069bd59b776b9f29b943711832e
pinky-pig <2545589993@qq.com>
2023-09-23 01:05:28 +0800

    fix: Alert 垂直水平居中

--- src/components/ui/Alert.vue
+++ src/components/ui/Alert.vue
@@ -30,7 +30,7 @@ defineExpose({
 </script>
 
 <template>
-  <div v-if="visible" class="scale-up-center alert-container pointer-events-none fixed h-full w-full flex items-center justify-center">
+  <div v-if="visible" class="alert-container scale-up-center pointer-events-none fixed bottom-0 left-0 right-0 top-0 h-full w-full flex items-center justify-center">
     <div class="h-64px w-200px flex flex-row items-center justify-center gap-2 rounded-md bg-white">
       <div i-fluent-emoji:check-mark />
       <span class="font-bold text-black">{{ info }}</span>
